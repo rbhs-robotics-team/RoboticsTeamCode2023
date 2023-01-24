d11bf4ef6a0b75acbbb1ea7fd2611f4a1524938c
Robotics Varsity Laptop <rbhsroboticsteam@gmail.com>
2023-01-24 16:13:53 -0600

    commit comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SleeveDetection.java
@@ -9,7 +9,7 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-// hello world
+// why does this not work?
 
 public class SleeveDetection extends OpenCvPipeline {
     /*
