27c8f65451f9d71ef2009f3441aca9a034dc252e
Robotics Varsity Laptop <rbhsroboticsteam@gmail.com>
2023-01-24 16:32:20 -0600

    no

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SleeveDetection.java
@@ -9,8 +9,6 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-// here is a comment
-
 public class SleeveDetection extends OpenCvPipeline {
     /*
     YELLOW  = Parking Left
