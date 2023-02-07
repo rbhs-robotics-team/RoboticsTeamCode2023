d370cf20a607894befe83e41eb19114eeb4f9a1d
Anastasia J Sokol <anastasia.j.sokol@gmail.com>
2023-02-06 18:13:53 -0600

    remove comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/LeftRoute.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/LeftRoute.java
@@ -1,17 +1,3 @@
-/**
- *  Things to consider
- *      a) The claw needs constant power the whole time (perhaps extract into a seperate class that runs in a seperate thread?)
- *      b) Moving and raising the claw can be done in parrellel (extract claw into a seperate, nonblocking, class - no extra thread needed)
- *      c) Perhaps straffing and moving forward can be done in parrellel, I am less sure about how to accomplish this
- *          - Similarly, it may be possible to simultaneously turn and move, more investigation would be needed
- *      d) With not insignificant work (perhaps it is too late in season) we could add a path finding algorithm
- *          - Allow for much easier autonomous coding
- *          - Allow for driver control shortcuts (press a button to go home, for example)
- *          - Allow for failure recovery (attempt to figure out where you are, perhaps by using the signal cone, then recover from there)
- *
- *          - Could sound cool if we explain how it works to the judges (which is perhaps our best shot at making state to be honest)
- **/
-
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
